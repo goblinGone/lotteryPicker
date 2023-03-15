e154493aac3d2ec7762b8374b67955ac02ad7709
goblinGone <j.conqueeftador@gmail.com>
2023-03-15 21:37:47 +0000

    is modular

--- lottoGen.cpp
+++ lottoGen.cpp
@@ -15,9 +15,6 @@
 using namespace std;
 using namespace std::chrono;
 
-int main () {
-system("clear");
-
 vector<int> lowNum;
 vector<int> highNum;
 vector<int> lotto;
@@ -30,19 +27,9 @@ auto loop_count = 0;
 bool isUnique = true;
 char selection {};
 
-	do {
-		
-		cout <<"*******************************"<<endl;
-		cout <<"Lotto num picker "<<endl;
-		cout <<"*******************************"<<endl;
-		cout <<"Generate Irish Lotto numbers - 1"<<endl;
-		cout <<"Quit - Q"<<endl;
-		cin >> selection;
-
-		switch (selection)
-		{
-		case '1':
-			while (is_even != 3 && isUnique){
+void picknum() {
+			while (is_even != 3)
+			{
 			is_even = 0;
 			loop_count = loop_count +1;
 			lowNum.clear();
@@ -81,25 +68,51 @@ char selection {};
 			//merges the two vectors into a third and prints the output
 			merge(lowNum.begin(), lowNum.end(), highNum.begin(), highNum.end(), back_inserter(lotto));
 			cout<<"DEBUG LOTTO SET :";
-			for (const int& j : lotto){
+			for (const int& j : lotto)
+			{
 				cout << j << " ";
-				}  
+			}  
 
-			for(auto i = 0; i < lotto.size(); i++){
-				if (lotto[i]%2==0){
+			for(auto i = 0; i < lotto.size(); i++)
+			{
+				if (lotto[i]%2==0)
+				{
 					is_even = is_even +1;
 				}
-				}
+			}
 				cout<<endl;
 				cout<<"DEBUG PRINT is_even :"<<is_even<<endl;
 				cout<<endl;
-			}
-			if (is_even == 3){
+			}	
+}
+
+
+
+int main () {
+
+	system("clear");
+
+	do {	
+		cout <<"*******************************"<<endl;
+		cout <<"Lotto num picker "<<endl;
+		cout <<"*******************************"<<endl;
+		cout <<"Generate Irish Lotto numbers - 1"<<endl;
+		cout <<"Quit - Q"<<endl;
+		cin >> selection;
+
+		switch (selection)
+		{
+		case '1':
+		do {
+			picknum();
+		} while (!isUnique);
+			
 				cout << "Some Potential reslts may be :";
-				for(const int&k:lotto){
+				for(const int&k:lotto)
+				{
 					cout << k << " ";
-					};
-			}
+				};
+
 			auto stop = high_resolution_clock::now();
 			auto duration = duration_cast<microseconds>(stop - start);
 			//DEBUGGING
@@ -108,13 +121,9 @@ char selection {};
 			cout <<"    Time taken :"<< duration.count() <<" microseconds"<<endl;
 			cout <<"    Loop count :"<<loop_count<<endl;
 			cout <<"    Even number count :" << is_even<<endl;
-
 			break;
-		
-
 		}
 	}
 	while (selection != 'q' && selection != 'Q');
-    
     return 0;
 }        
